77f84f29ecbfd04fe8e84ca7c65210f35bb3dc0a
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-09 15:06:47 +0100

    Add ciclo while e 3 possibili soluzioni

--- js/script.js
+++ js/script.js
@@ -5,3 +5,33 @@
         3. Scrivere il ciclo while
             3a. All'interno del ciclo, scrivere il comando che inserirà gli elementi della lista nel documento
 */
+
+const myList = [
+    "pane",
+    "acqua",
+    "verdura",
+    "pesce",
+    "carne",
+    "lievito",
+    "farina",
+    "zucchero"
+];
+
+let i = 0;
+
+const itemsList = document.getElementById("items");
+
+while (i < myList.length) {
+    // document.writeln(myList[i] + "<br>");
+    // console.log("lista", myList[i]);
+
+    // let newLi = document.createElement("li");
+    // newLi.innerHTML = myList[i];
+    // itemsList.append(newLi);
+    // console.log("lista", myList[i]);
+
+    itemsList.innerHTML += `<li>${myList[i]}</li>`;
+    console.log("lista", myList[i]);
+
+    i++;
+}
