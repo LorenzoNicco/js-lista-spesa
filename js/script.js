732784d921950688ac28d65512fbc534709f2850
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-09 14:40:18 +0100

    Inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+    STEPS:
+        1. Scrivere la lista della spesa in un array
+        2. Inizializzare la variabile di riferimento per il ciclo.
+        3. Scrivere il ciclo while
+            3a. All'interno del ciclo, scrivere il comando che inserirà gli elementi della lista nel documento
+*/
